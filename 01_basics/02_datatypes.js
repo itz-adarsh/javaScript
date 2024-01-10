29518417b7cf4cb53c3173e88db7ef363d58bb50
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-10 17:25:11 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("3+3") // we are using nodejs, not browser 
+
+//  console.log(3
+    // +3) // code readability should be high
+
+    let name = "hitesh"
+    let age = 18
+    let isloggedIn = false
+
+/***************** primtive data type *************/
+
+    // number => 2 to power 53
+    // bigint
+    // string =>""or ''
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+    // object
+
+    console.log(typeof "adarsh"); //type of is used to detect the data type
+    console.log(typeof age);
+    console.log(typeof null); // data type of null is object
+    console.log(typeof undefined); // type is undefined 
