73bd7ef2c39753789d085ac940df275dc7aa4464
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-11 04:53:02 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"   // "33"=>33
+// let score = "33abc" // "33abc"=> NaN 
+// let score = null    
+// let score = undefined
+// let score = true // true = 1; false = 0
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// let isLoggedIn = 1
+
+// let isLoggedIn = ""
+// let isLoggedIn = "adarsh" 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+ 
+
+//the boolean conversion of a non-empty string i.e ("adarsh") results in true and empty string ("") in false
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
