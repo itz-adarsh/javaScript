ec874653875d720f777979be33da13dd5c4035dd
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-18 08:46:20 +0000

    symbol

---
+++ 01_basics/summary.js
@@ -0,0 +1,15 @@
+// primitive call by value hota hai
+
+//  7 types : String, Number, boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+// refrence (non primitive)
+//   Array, Objects, Functions
