811d39b38d45d356e413f2d62311933c2c191f78
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-18 08:33:04 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -9,7 +9,7 @@ console.log("01">=false)
 // yha pe phle js isko same dattype me automatic convert kr deta hai fir comapare krta hai
 
 console.log(null > 0)
-console.log(null == 0)
+console.log(null == 0)//This will output false as well. The equality (==) operator performs type coercion, and in this case, null is not equal to 0. Type coercion does not convert null to a numeric value
 console.log(null >= 0)
 // the reason is that an equality check == and comparison > < >= <= work differently. comparison converts null to a number, treating it as 0. that's why (3) null>=0 is true and (1) null<=0 is false 
 
