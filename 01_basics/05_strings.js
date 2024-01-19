e016ec57496c563cdcaa843cb8def20994058884
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-19 15:24:31 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "adarsh"
+const repoCount = 50
+
+// console.log(name + repoCount + "  value"); this is not looking good so we use lower mwethod
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
