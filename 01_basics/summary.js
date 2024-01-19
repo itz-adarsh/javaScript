4c938dd41f7e00008c7ce749209827e9913a72a9
itz-adarsh <155943727+itz-adarsh@users.noreply.github.com>
2024-01-19 14:38:32 +0000

    summary

--- 01_basics/summary.js
+++ 01_basics/summary.js
@@ -38,3 +38,37 @@ const myFunction = function(){
 
 // refrence (non primitive)
 //   Array, Objects, Functions
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack memory (primtive) , heap memory (non-primtive)
+
+let myYoutubename = "hitshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chai aur code"
+console.log(myYoutubename)
+console.log(anothername);
+
+// heap example 
+
+let userOne = {
+    email: "user@gamil.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "adarshkumar@gmail.com"
+userTwo.upi = "adarsh@ybl"
+console.log(userOne.email);
+console.log(userOne.upi);
+console.log(userTwo.email);
+console.log(userTwo.upi);
+
+
+// stack memory ke andr jb bhi koi ek chiz lenge to uska ek copy hi milta hai due to this change krne pe copy me hi chnage hota hai na ki original me 
+
+// lekin heap ke andr refrence milta hai means actually jo bhi changes krte hai original value ke andr hi krte hai
